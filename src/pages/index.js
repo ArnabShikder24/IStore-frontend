4f66a54fe913bec0fa0ad981dfdb0e19f9aafacf
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-12-03 22:04:47 +0600

    Marqueue

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,9 @@ export default function Home() {
     <main>
       <h1 className="text-5xl text-center">Hello !!!</h1>
       <h1 className="text-5xl text-center">This is Home page</h1>
+      <marquee behavior="scroll" direction="left" className="mt-2">
+        This text is scrolling from right to left.
+      </marquee>
     </main>
   );
 }
