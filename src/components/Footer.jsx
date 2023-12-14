531af77e26804aac00f3d2e55a17b8aeae6db020
ArnabShikder24 <arnabshikder2001@gmail.com>
2023-12-14 15:40:04 +0600

    navbar banner setup

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='py-3 text-center bg-[#7033ff] text-white text-sm'>
+            @Copyright by IStore 2024
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/pages/index.jsx
@@ -0,0 +1,27 @@
+import RootLayout from "@/components/RootLayout";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <section id="hero" className='container mx-auto mb-24 grid grid-cols-1 lg:grid-cols-2 place-items-center px-4'>
+        <div>
+          <h1 className='font-semibold text-4xl md:text-6xl text-center lg:text-left'>Your Trusted Destination for iProducts</h1>
+          <Image src={'/png/iphoneBanner.png'} width={450} height={540} alt="hero section" className='lg:hidden mx-auto my-8' />
+          <p className='paragraph text-center lg:text-left my-8'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti odio accusantium nulla! Magnam laudantium et, adipisci nostrum modi quae vero?</p>
+  
+          <button
+            className='py-3 px-6 bg-[#8338ec] text-white rounded font-bold capitalize block mx-auto lg:m-0'
+          >get in touch</button>
+        </div>
+        <Image src={'/png/iphoneBanner.png'} width={450} height={540} alt="hero section" className='hidden lg:block' />
+      </section>
+    </>
+  );
+}
+
+Home.getLayout = function getLayout(page) {
+  return (
+    <RootLayout>{page}</RootLayout>
+  )
+}
