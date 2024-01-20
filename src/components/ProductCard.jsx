f64f2f0544cd0bc764c2b943b4f6736eb8d9405b
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2024-01-20 23:07:14 +0600

    Product Card Staticall Ready

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export const ProductCard = () => {
   return (
     <>
-      <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-2xl dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
           <Image
             width={450}
@@ -75,6 +75,12 @@ export const ProductCard = () => {
             <span class="text-3xl font-bold text-gray-900 dark:text-white">
               $599
             </span>
+          </div>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            eveniet numquam vero tenetur, doloribus earum atque.
+          </div>
+          <div className="mt-4">
             <a
               href="#"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
