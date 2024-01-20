51f2e5423fa0fc2ec8d10eb72f29a1c3c30aa98b
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2024-01-20 22:09:54 +0600

    Flowbite Installation

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const ProductCard = () => {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum nostrum
+        in optio, quisquam commodi velit atque ab vel incidunt neque, facere
+        aliquam eum dignissimos ipsa exercitationem laboriosam culpa nulla omnis
+        quas illum fugiat non excepturi dicta cum. Sit deserunt, quasi nesciunt
+        perferendis obcaecati tempore explicabo nulla amet eligendi voluptates
+        consequatur.
+      </h1>
+    </div>
+  );
+};
