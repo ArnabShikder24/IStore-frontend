48114d5080b69cdf6b93472fb93c06c5500f7254
ArnabShikder24 <arnabshikder2001@gmail.com>
2024-02-12 11:19:04 +0600

    total amount counted

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -11,8 +11,21 @@ export default function Cart() {
     const [user, setUser] = useState(null);
     const [updateSuccess, setUpdateSuccess] = useState('');
     const [loading, setLoading] = useState(true);
+    const [mainTotal, setMainTotal] = useState(0);
 
-    console.log(orders);
+    useEffect(() => {
+        setLoading(true)
+        const calculateMainTotal = () => {
+            const total = orders.reduce((accumulator, currentOrder) => {
+            return accumulator + parseFloat(currentOrder.subtotal);
+            }, 0);
+    
+            setMainTotal(total.toFixed(2));
+            setLoading(false)
+        };
+        calculateMainTotal();
+    }, [orders]);
+    
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
         setUser(user);
@@ -22,7 +35,6 @@ export default function Cart() {
     return () => unsubscribe();
     }, []);
 
-    
 
     useEffect(() => {
         const fetchData = async (usere) => {
@@ -50,7 +62,16 @@ export default function Cart() {
             <div className="grid grid-cols-3 gap-20">
                 {orders.map((order, idx) => <OrderCard order={order} setUpdateSuccess={setUpdateSuccess} key={idx} />)}
             </div>
-        </div>
+            </div>
+            <div className="text-center mt-20 py-10">
+                <h1 className="text-xl font-bold">Your Total Amount</h1>
+                <p className="text-xl font-bold py-4">${mainTotal}</p>
+                <button
+                      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      Confirm Order
+                    </button>
+            </div>
         </div>
     );
 }
